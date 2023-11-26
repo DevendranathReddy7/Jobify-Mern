8d6fec14719347402ac85aa6f67208f828d167d3
Devendra <devendranathreddy54@gmail.com>
2023-11-26 10:22:32 +0530

    added login componrnt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
+import { Logo, RegisterForm } from "../components"
+
 const Login = () => {
     return (
-        <div>
-        </div>
+        <Wrapper>
+            <form className="form">
+                <Logo />
+                <h4>Login</h4>
+                <RegisterForm type='email' name='email' defaultValue='dev@gmail.com' />
+                <RegisterForm type='password' name='password' defaultValue='password123' />
+                <button type="submit" className="btn btn-block">Submit</button>
+                <button type="submit" className="btn btn-block">Login as Guest</button>
+                <p>Not a member yet? <Link to='/register' className="member-btn">Register</Link></p>
+
+            </form>
+        </Wrapper>
     )
 }
 export default Login
