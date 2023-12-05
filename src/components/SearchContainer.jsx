25325f058c5c5b23eac22419cc71c338548dd893
Devendra <devendranathreddy54@gmail.com>
2023-12-05 20:19:28 +0530

    adding stats container

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,8 +1,36 @@
+import { RegisterForm, FormSelect, SubmitBtn } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { Form, useSubmit, Link } from 'react-router-dom';
+import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from '../../../utils/constants';
+import { useAllJobsContext } from '../pages/AllJobs';
+
 const SearchContainer = () => {
+    const { searchValues } = useAllJobsContext()
+    const { search, jobStatus, jobType, sort } = searchValues;
+    const debounce = (onChange) => {
+        let timeout;
+        return (e) => {
+            const form = e.currentTarget.form;
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                onChange(form);
+            }, 2000);
+        };
+    };
+    const submit = useSubmit()
     return (
-        <div>
-            <h3>Search</h3>
-        </div>
+        <Wrapper>
+            <Form className='form'>
+                <h5 className='form-title'>Search</h5>
+                <div className="form-center">
+                    <RegisterForm type='search' name='search' defaultValue={search} onChange={debounce((form) => { submit(form) })} />
+                    <FormSelect labelText='job status' name='jobStatus' list={['all', ...Object.values(JOB_STATUS)]} defaultValue={jobStatus} onChange={(e) => { submit(e.currentTarget.form) }} />
+                    <FormSelect labelText='job type' name='jobType' list={['all', ...Object.values(JOB_TYPE)]} defaultValue={jobType} onChange={(e) => { submit(e.currentTarget.form) }} />
+                    <FormSelect name='sort' list={[...Object.values(JOB_SORT_BY)]} defaultValue={sort} onChange={(e) => { submit(e.currentTarget.form) }} />
+                    <Link to="/dashboard/all-jobs" className='btn form-btn delete-btn'>Reset Search</Link>
+                </div>
+            </Form>
+        </Wrapper>
     )
 }
 export default SearchContainer
